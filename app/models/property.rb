a02a4ab6c0fb573ee1cd3a4c5c7cca79e9e92dbf
DaniloJNS <dan.silva.13.zx@gmail.com>
2021-10-03 11:02:16 -0300

    add unit test property

--- app/models/property.rb
+++ app/models/property.rb
@@ -3,5 +3,7 @@ class Property < ApplicationRecord
     belongs_to :property_location
     validates :title, :description, :rooms, :bathrooms, :daily_rate, 
         presence: {message: 'não pode ficar em branco'}
+    validates :rooms, :bathrooms, :numericality => {:only_integer => true, :greater_than => 0}
+    validates :daily_rate, :numericality => { :greater_than => 0 }
 end
 
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -1,11 +1,78 @@
 require 'rails_helper'
 
 describe Property do
+
   context 'validations' do
-    it 'name must be present' do
-      property = Property.new
-      property.valid?
-      expect(property.errors.full_messages_for(:title)).to include('Título não pode ficar em branco') 
+
+    context 'present' do
+      let(:property) { subject }
+
+      it 'title must be present' do
+        property.valid?
+        expect(property.errors.full_messages_for(:title)).to include('Título não pode ficar em branco') 
+      end
+
+      it 'description must be present' do
+        property.valid?
+        expect(property.errors.full_messages_for(:description)).to include('Descrição não pode ficar em branco') 
+      end
+
+      it 'rooms must be present' do
+        property.valid?
+        expect(property.errors.full_messages_for(:rooms)).to include('Quartos não pode ficar em branco') 
+      end
+
+      it 'bathrooms must be present' do
+        property.valid?
+        expect(property.errors.full_messages_for(:bathrooms)).to include('Banheiros não pode ficar em branco') 
+      end
+
+      it 'daily_rate must be present' do
+        property.valid?
+        expect(property.errors.full_messages_for(:daily_rate)).to include('Diária não pode ficar em branco') 
+      end
     end
+
+    context 'must be greater than 0' do
+      let(:property) { subject }
+
+      it 'rooms must be greater than 0' do
+        property.rooms = -1
+        property.valid?
+        expect(property.errors.full_messages_for(:rooms)).to include("Quartos deve ser maior que 0") 
+      end
+
+      it 'bathrooms must be greater than 0' do
+        property.bathrooms = -1
+        property.valid?
+        expect(property.errors.full_messages_for(:bathrooms)).to include("Banheiros deve ser maior que 0") 
+      end
+
+      it 'daily_rate must be greater than 0' do
+        property.daily_rate = -1
+        property.valid?
+        expect(property.errors.full_messages_for(:daily_rate)).to include("Diária deve ser maior que 0") 
+      end
+
+      context 'must be integer' do
+        let(:property) { subject }
+
+        it 'rooms must be integer' do
+          property.rooms = 1.2
+          property.valid?
+          expect(property.errors.full_messages_for(:rooms)).to include("Quartos não é um número inteiro") 
+        end
+
+        it 'bathrooms must be integer' do
+          property.bathrooms = 1.2
+          property.valid?
+          expect(property.errors.full_messages_for(:bathrooms)).to include("Banheiros não é um número inteiro") 
+        end
+
+      end
+
+    end
+
   end
+
 end
